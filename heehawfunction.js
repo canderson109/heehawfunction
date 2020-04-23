ab336c17aea14bea1a313a113fcd5ffa0a7244d4
name <canderson109@mail.greenriver.edu>
2020-04-23 02:40:56 -0400

    added type check

--- heehawfunction.js
+++ heehawfunction.js
@@ -3,9 +3,13 @@
 function heehaw() {
     let input = document.getElementById('input');
 
-    console.log("test")
+    if (isNaN(input.value)) {
+        console.log("Error: Did not receive numerical input")
+        document.write("<h4>Error: You must input a number!</h4>");
+    }
 
     for (i = 1; i <= input.value; i++) {
+
         if (i % 3 == 0 && i % 5 == 0) {
             document.write("<h4>Hee Haw!</h4>");
         } else if (i % 3 == 0) {
